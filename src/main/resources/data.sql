c65b947a6a0f706282993678bd9f4eecf4f0b8c4
Aadya Garg <aadyagarg245@gmail.com>
2026-04-20 15:18:59 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, description, brand, price, category, release_date, available, quantity)
+VALUES ('Tata Nexon', 'A compact Suv', 'Tata', 750000.00, 'Cars', '2024-01-15', true, 50),
+       ('Maruti Suzuki', 'A popular hatchback', 'Maruti', 550000.00, 'Cars', '2024-02-01', true, 100);
